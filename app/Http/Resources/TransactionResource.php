e6d690db5d063b5164bc3588ff8e6234c54520a7
Monalisa Alencar <monalisa.alencar@pede.ai>
2022-07-11 03:39:25 -0300

    feat:  create resourcer to transactions

---
+++ app/Http/Resources/TransactionResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TransactionResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'total_value' => $this->total_value,
+            'payer_id' => $this->payer_id,
+            'payee_id' => $this->payee_id
+        ];
+    }
+}
