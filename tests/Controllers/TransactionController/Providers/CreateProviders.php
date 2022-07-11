ec6d1b70a50fc107d7ce00a8b8911d6d530c134b
Monalisa Alencar <monalisa.alencar@pede.ai>
2022-07-11 03:32:56 -0300

    feat: add providers to transaction test

---
+++ tests/Controllers/TransactionController/Providers/CreateProviders.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Controllers\TransactionController\Providers;
+
+trait CreateProviders
+{
+    public function payload()
+    {
+        return [
+            'total value require' => [
+                'payload' => [
+                        'payer_id' => '62cb68c9b18d8913e226f1eq',
+                        'payer_id' => '62cb68c9b18d8913e226f1eq',
+                ],
+                'status' => 422,
+            ],
+            'payer_id require' => [
+                'payload' => [
+                    'total_value' => '84002695000',
+                    'payer_id' => '62cb68c9b18d8913e226f1eq',
+                ],
+                'status' => 422,
+            ],
+            'payee_id require' => [
+                'payload' => [
+                    'total_value' => '84002695000',
+                    'payer_id' => '62cb68c9b18d8913e226f1eq',
+                ],
+                'status' => 422,
+            ]
+        ];
+    }
+}
