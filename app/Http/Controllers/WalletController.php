a347caea7bd0b5b9f2a6a58ee78782298c076be4
Monalisa Alencar <monalisa.alencar@pede.ai>
2022-07-11 03:37:47 -0300

    feat: add wallet controller

---
+++ app/Http/Controllers/WalletController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Wallet;
+use Illuminate\Http\Request;
+
+class WalletController extends Controller
+{
+    /**
+     * cria carteira digital para o cliente
+     *
+     * @param Request $request  Requisição
+     * @return void
+     */
+    public function create(Request $request)
+    {
+        $new_wallet = Wallet::create([
+            'client_id' => $request['client_id'],
+            'balance_value' => $request['balance_value']
+        ]);
+
+        return response([
+            'transaction' => [
+                'id' => $new_wallet->_id,
+            ],
+            'message' => 'Nova carteira criada com sucesso!',
+        ], 201);
+    }
+
+    /**
+     * exibe a dados da carteira do client
+     *
+     * @param string $id identificador do cliente
+     * @return void
+     */
+    public function show(string $id)
+    {
+        $wallet = Wallet::where('client_id', $id)
+            ->with('client')
+            ->first();
+
+        if (empty($wallet)) {
+            return response([
+                'message' => 'Carteira não encontrada',
+            ], 400);
+        }
+
+        return response([
+            'wallet' => $wallet->toArray(),
+        ], 200);
+    }
+}
