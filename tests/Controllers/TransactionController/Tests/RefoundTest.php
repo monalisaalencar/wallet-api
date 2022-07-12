4e1efec4bc7f39690b08cbfc5f0d6ab91ace2f22
Monalisa Alencar <monalisa.alencar@pede.ai>
2022-07-11 21:56:05 -0300

    test: test refactor applying clean code

--- tests/Controllers/TransactionController/Tests/RefoundTest.php
+++ tests/Controllers/TransactionController/Tests/RefoundTest.php
@@ -15,6 +15,23 @@ class RefoundTest extends TestCase
     protected function setUp(): void
     {
         parent::setUp();
+        $this->clients = Client::factory(2)->create([
+            'type' => 'common'
+        ]);
+        foreach ($this->clients as $client) {
+            Wallet::factory()->create([
+                'balance_value' => 10000,
+                'client_id' => $client->_id
+            ]);
+        }
+        $this->payee_id = $this->clients[0]->id;
+        $this->payer_id = $this->clients[1]->id;
+
+        $this->transaction = Transaction::factory()->create([
+            'payee_id' => $this->payee_id,
+            'payer_id' => $this->payer_id,
+            'total_value' => 200
+        ]);
     }
 
     /**
@@ -36,36 +53,46 @@ class RefoundTest extends TestCase
     /**
      * @test
      */
-    public function returnedFields()
+    public function returnedFieldsWithValuesValid()
     {
-        $clients = Client::factory(2)->create([
-            'type' => 'common'
-        ]);
-        foreach ($clients as $client) {
-            Wallet::factory()->create([
-                'balance_value' => 10000,
-                'client_id' => $client->_id
-            ]);
-        }
-        $transaction = Transaction::factory()->create([
-            'payee_id' => $clients[0]->id,
-            'payer_id' => $clients[1]->id
-        ]);
         $this->json(
             $this->http_verb,
-            $this->route . "/{$transaction->_id}",
+            $this->route . "/{$this->transaction->_id}",
         );
+
         $this->assertResponseOk();
 
-        $transaction->forceDelete();
-        foreach ($clients as $client) {
-            $client->forceDelete();
-        }
-        Wallet::where('client_id', $clients->pluck('_id')->toArray())->delete();
+        $wallet_payer = Wallet::where('client_id', $this->payer_id)->first();
+        $this->assertEquals(10000 + 200, $wallet_payer->balance_value);
+
+        $wallet_payee = Wallet::where('client_id', $this->payee_id)->first();
+        $this->assertEquals(10000 - 200, $wallet_payee->balance_value);
+    }
+
+    /**
+     * @test
+     */
+    public function replyWithSuccessMessage()
+    {
+        $response = $this->json(
+            $this->http_verb,
+            $this->route . "/{$this->transaction->_id}",
+        );
+
+        $this->assertResponseOk();
+
+        $content = json_decode($response->response->content(), true);
+
+        $this->assertEquals($content['message'], 'Transação reembolsada com sucesso!');
     }
 
     protected function tearDown(): void
     {
+        Wallet::whereIn('client_id', $this->clients->pluck('_id')->toArray())->forceDelete();
+        $this->transaction->forceDelete();
+        foreach ($this->clients as $client) {
+            $client->forceDelete();
+        }
         parent::tearDown();
     }
 }
